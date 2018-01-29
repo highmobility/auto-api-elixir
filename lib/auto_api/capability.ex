bc136a2aef86483947058d91acad81553e78d240
Kevin Valdek <kevinvaldek@gmail.com>
2018-01-29 21:29:02 +0100

    added keyfob position capability.

--- lib/auto_api/capability.ex
+++ lib/auto_api/capability.ex
@@ -260,7 +260,8 @@ defmodule AutoApi.Capability do
     <<0x00, 0x40>> => AutoApi.FuelingCapability,
     <<0x00, 0x52>> => AutoApi.GraphicsCapability,
     <<0x00, 0x29>> => AutoApi.HeartRateCapability,
-    <<0x00, 0x60>> => AutoApi.HomeChargerCapability
+    <<0x00, 0x60>> => AutoApi.HomeChargerCapability,
+    <<0x00, 0x48>> => AutoApi.KeyfobPositionCapability
   }
 
   @doc """
