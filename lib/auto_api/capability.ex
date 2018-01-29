deca8105f26377db7381053b29694d94c8dddc45
Kevin Valdek <kevinvaldek@gmail.com>
2018-01-29 21:04:44 +0100

    added climate capability.

--- lib/auto_api/capability.ex
+++ lib/auto_api/capability.ex
@@ -253,7 +253,8 @@ defmodule AutoApi.Capability do
     <<0x00, 0x34>> => AutoApi.MaintenanceCapability,
     <<0x00, 0x49>> => AutoApi.BrowserCapability,
     <<0x00, 0x23>> => AutoApi.ChargingCapability,
-    <<0x00, 0x53>> => AutoApi.ChassisSettingsCapability
+    <<0x00, 0x53>> => AutoApi.ChassisSettingsCapability,
+    <<0x00, 0x24>> => AutoApi.ClimateCapability
   }
 
   @doc """
