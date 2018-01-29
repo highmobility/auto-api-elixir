8947efcd9391775175ae53e7df5a9d3a35e7ffe2
Kevin Valdek <kevinvaldek@gmail.com>
2018-01-29 20:38:42 +0100

    added seats capability.

---
+++ lib/auto_api/capabilities/seats_capability.ex
@@ -0,0 +1,47 @@
+# AutoAPI
+# Copyright (C) 2018 High-Mobility GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# Please inquire about commercial licensing options at
+# licensing@high-mobility.com
+defmodule AutoApi.SeatsCapability do
+  @moduledoc """
+  Basic settings for Seats Capability
+
+      iex> alias AutoApi.SeatsCapability, as: S
+      iex> S.identifier
+      <<0x00, 0x56>>
+      iex> S.name
+      :seats
+      iex> S.description
+      "Seats"
+      iex> S.command_name(0x00)
+      :get_seats_state
+      iex> S.command_name(0x01)
+      :seats_state
+      iex> length(S.properties)
+      1
+      iex> List.last(S.properties)
+      {0x01, :seat}
+  """
+
+  @spec_file "specs/seats.json"
+  @type command_type :: :get_seats_state | :seats_state
+
+  @command_module AutoApi.NotImplemented
+  @state_module AutoApi.NotImplemented
+
+  use AutoApi.Capability
+end
--- lib/auto_api/capability.ex
+++ lib/auto_api/capability.ex
@@ -222,7 +222,8 @@ defmodule AutoApi.Capability do
   @capabilities %{
     <<0x00, 0x10>> => AutoApi.CapabilitiesCapability,
     <<0x00, 0x33>> => AutoApi.DiagnosticsCapability,
-    <<0x00, 0x20>> => AutoApi.DoorLocksCapability
+    <<0x00, 0x20>> => AutoApi.DoorLocksCapability,
+    <<0x00, 0x56>> => AutoApi.SeatsCapability
   }
 
   @doc """
