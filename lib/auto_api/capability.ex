68da8b6ba4fff8eb4b9b09446d90bd2b342921fe
Milad Rastian <milad@high-mobility.com>
2018-02-19 09:59:18 +0100

    add firmware version capability module

--- lib/auto_api/capability.ex
+++ lib/auto_api/capability.ex
@@ -229,6 +229,7 @@ defmodule AutoApi.Capability do
   end
 
   @capabilities %{
+    <<0x00, 0x03>> => AutoApi.FirmwareVersionCapability,
     <<0x00, 0x10>> => AutoApi.CapabilitiesCapability,
     <<0x00, 0x33>> => AutoApi.DiagnosticsCapability,
     <<0x00, 0x20>> => AutoApi.DoorLocksCapability,
