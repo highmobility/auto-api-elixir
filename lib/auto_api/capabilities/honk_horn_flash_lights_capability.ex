70ca71ee83f8dbe280266471393a42aade0dd12e
Milad Rastian <eslashmili@gmail.com>
2018-05-24 16:47:13 +0200

    Fix honk state and command setting

--- lib/auto_api/capabilities/honk_horn_flash_lights_capability.ex
+++ lib/auto_api/capabilities/honk_horn_flash_lights_capability.ex
@@ -46,7 +46,7 @@ defmodule AutoApi.HonkHornFlashLightsCapability do
           | :honk_flash
           | :activate_deactivate_emergency_flashers
 
-  @command_module AutoApi.HonkHornFlashLightsState
-  @state_module AutoApi.HonkHornFlashLightsCommand
+  @command_module AutoApi.HonkHornFlashLightsCommand
+  @state_module AutoApi.HonkHornFlashLightsState
   use AutoApi.Capability
 end
