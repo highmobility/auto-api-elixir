81b4475d439aa320a25ce61cd05ae771f88e714b
Milad Rastian <eslashmili@gmail.com>
2018-11-02 16:57:56 +0100

    Update vehicle_location_capability

--- lib/auto_api/capabilities/vehicle_location_capability.ex
+++ lib/auto_api/capabilities/vehicle_location_capability.ex
@@ -40,7 +40,7 @@ defmodule AutoApi.VehicleLocationCapability do
   @spec_file "specs/vehicle_location.json"
   @type command_type :: :vehicle_location | :get_vehicle_location
 
-  @command_module AutoApi.NotImplemented
+  @command_module AutoApi.VehicleLocationCommand
   @state_module AutoApi.NotImplemented
 
   use AutoApi.Capability
