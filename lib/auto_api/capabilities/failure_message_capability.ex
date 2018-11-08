02a3c7de9b39b16051a7fb0b28a5854d098cc6f6
Milad Rastian <eslashmili@gmail.com>
2018-11-08 06:48:44 +0100

    Add failure message capability

---
+++ lib/auto_api/capabilities/failure_message_capability.ex
@@ -0,0 +1,45 @@
+# AutoAPI
+# Copyright (C) 2018 High-Mobility GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# Please inquire about commercial licensing options at
+# licensing@high-mobility.com
+defmodule AutoApi.FailureMessageCapability do
+  @moduledoc """
+  Basic settings for FailureMessage Capability
+
+      iex> alias AutoApi.FailureMessageCapability, as: F
+      iex> F.identifier
+      <<0x00, 0x2>>
+      iex> F.name
+      :failure_message
+      iex> F.description
+      "Failure Message"
+      iex> F.command_name(0x01)
+      :failure
+      iex> length(F.properties)
+      4
+      iex> List.last(F.properties)
+      {4, :failure_description}
+  """
+
+  @spec_file "specs/failure_message.json"
+  @type command_type :: :failure_message
+
+  @command_module AutoApi.FailureMessageCommand
+  @state_module AutoApi.FailureMessageState
+
+  use AutoApi.Capability
+end
