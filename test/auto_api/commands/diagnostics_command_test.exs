f49761513f422df712279c01522499d6fdb3a759
Milad Rastian <eslashmili@gmail.com>
2019-02-21 15:56:36 +0100

    Update diagnostics to level10

--- test/auto_api/commands/diagnostics_command_test.exs
+++ test/auto_api/commands/diagnostics_command_test.exs
@@ -18,5 +18,29 @@
 # licensing@high-mobility.com
 defmodule AutoApi.DiagnosticsCommandTest do
   use ExUnit.Case
-  doctest AutoApi.DiagnosticsCommand
+  alias AutoApi.{DiagnosticsCommand, DiagnosticsState, PropertyComponent}
+
+  describe "execute/2" do
+    test "get_diagnostics_state" do
+      assert DiagnosticsCommand.execute(%DiagnosticsState{}, <<0x00>>) ==
+               {:state, %AutoApi.DiagnosticsState{}}
+    end
+
+    test "diagnostics_state" do
+      mileage_prop = %PropertyComponent{data: 100}
+
+      binary_state = DiagnosticsState.to_bin(%DiagnosticsState{mileage: mileage_prop})
+
+      assert DiagnosticsCommand.execute(%DiagnosticsState{}, <<0x01, binary_state::binary>>) ==
+               {:state_changed, %AutoApi.DiagnosticsState{mileage: mileage_prop}}
+    end
+  end
+
+  describe "state/1" do
+    test "get state" do
+      state = %DiagnosticsState{mileage: %PropertyComponent{data: 100}}
+
+      assert DiagnosticsCommand.state(state) == <<0x01>> <> DiagnosticsState.to_bin(state)
+    end
+  end
 end
--- test/auto_api/states/diagnostics_state_test.exs
+++ test/auto_api/states/diagnostics_state_test.exs
@@ -18,5 +18,67 @@
 # licensing@high-mobility.com
 defmodule AutoApi.DiagnosticsStateTest do
   use ExUnit.Case
-  doctest AutoApi.DiagnosticsState
+  alias AutoApi.{PropertyComponent, DiagnosticsState}
+  doctest DiagnosticsState
+
+  test "to_bin & from_bin" do
+    state = %DiagnosticsState{
+      mileage: %PropertyComponent{data: 10},
+      engine_oil_temperature: %PropertyComponent{data: 2},
+      speed: %PropertyComponent{data: 3},
+      engine_rpm: %PropertyComponent{data: 4},
+      fuel_level: %PropertyComponent{data: 1.0001},
+      estimated_range: %PropertyComponent{data: 5},
+      washer_fluid_level: %PropertyComponent{data: :filled},
+      battery_voltage: %PropertyComponent{data: 1.003},
+      distance_since_reset: %PropertyComponent{data: 6},
+      distance_since_start: %PropertyComponent{data: 7},
+      fuel_volume: %PropertyComponent{data: 1.004},
+      anti_lock_braking: %PropertyComponent{data: :inactive},
+      engine_coolant_temperature: %PropertyComponent{data: 8},
+      engine_total_operating_hours: %PropertyComponent{data: 1.005},
+      engine_total_fuel_consumption: %PropertyComponent{data: 1.006},
+      brake_fluid_level: %PropertyComponent{data: :low},
+      engine_torque: %PropertyComponent{data: 1.007},
+      engine_load: %PropertyComponent{data: 1.008},
+      wheel_based_speed: %PropertyComponent{data: 9},
+      battery_level: %PropertyComponent{data: 1.009},
+      check_control_messages: [
+        %PropertyComponent{
+          data: %{
+            id: 1,
+            remaining_minutes: 300,
+            text_size: 4,
+            text: "text",
+            status_size: 6,
+            status: "status"
+          }
+        }
+      ],
+      tire_pressures: [%PropertyComponent{data: %{location: :rear_left, pressure: 1.009}}],
+      tire_temperatures: [%PropertyComponent{data: %{location: :rear_left, temperature: 1.010}}],
+      wheel_rpms: [%PropertyComponent{data: %{location: :rear_right, rpm: 10}}],
+      trouble_codes: [
+        %PropertyComponent{
+          data: %{
+            occurences: 1,
+            id_size: 2,
+            id: "id",
+            ecu_id_size: 6,
+            ecu_id: "ecu_id",
+            status_size: 6,
+            status: "status"
+          }
+        }
+      ],
+      mileage_meters: %PropertyComponent{data: 11}
+    }
+
+    new_state =
+      state
+      |> DiagnosticsState.to_bin()
+      |> DiagnosticsState.from_bin()
+
+    assert new_state == state
+  end
 end
