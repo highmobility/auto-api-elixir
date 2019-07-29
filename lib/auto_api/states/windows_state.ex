c8efed7a19fddf1c6d55438e23b0934bf00f3269
Francesco Donadon <francesco.donadon@gmail.com>
2019-07-29 13:02:07 +0200

    Update Windows state and tests

--- lib/auto_api/states/windows_state.ex
+++ lib/auto_api/states/windows_state.ex
@@ -35,10 +35,10 @@ defmodule AutoApi.WindowsState do
   @type location :: :front_left | :front_right | :rear_left | :rear_right | :hatch
   @type position :: :close | :open | :intermediate
   @type window_position :: %PropertyComponent{
-          data: %{window_location: location, window_position: position}
+          data: %{location: location, window_position: position}
         }
   @type window_open_percentage :: %PropertyComponent{
-          data: %{window_location: location, open_percentage: float}
+          data: %{location: location, open_percentage: float}
         }
 
   @type t :: %__MODULE__{
@@ -52,7 +52,7 @@ defmodule AutoApi.WindowsState do
 
     iex> bin = <<2, 0, 12, 1, 0, 9, 4, 63, 199, 10, 61, 112, 163, 215, 10>>
     iex> AutoApi.WindowsState.from_bin(bin)
-    %AutoApi.WindowsState{windows_open_percentages: [%AutoApi.PropertyComponent{data: %{window_location: :hatch, open_percentage: 0.18}}]}
+    %AutoApi.WindowsState{windows_open_percentages: [%AutoApi.PropertyComponent{data: %{location: :hatch, open_percentage: 0.18}}]}
   """
   @spec from_bin(binary) :: __MODULE__.t()
   def from_bin(bin) do
@@ -62,7 +62,7 @@ defmodule AutoApi.WindowsState do
   @doc """
   Parse state to bin
 
-    iex> state = %AutoApi.WindowsState{windows_open_percentages: [%AutoApi.PropertyComponent{data: %{window_location: :hatch, open_percentage: 0.18}}]}
+    iex> state = %AutoApi.WindowsState{windows_open_percentages: [%AutoApi.PropertyComponent{data: %{location: :hatch, open_percentage: 0.18}}]}
     iex> AutoApi.WindowsState.to_bin(state)
     <<2, 0, 12, 1, 0, 9, 4, 63, 199, 10, 61, 112, 163, 215, 10>>
   """
