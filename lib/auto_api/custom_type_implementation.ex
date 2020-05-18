ac9994f8d121105f136a3bd05ea76d3c85fb3d38
Francesco Donadon <francesco.donadon@gmail.com>
2020-05-18 10:05:54 +0200

    CustomType point to correct spec path

--- lib/auto_api/custom_type_implementation.ex
+++ lib/auto_api/custom_type_implementation.ex
@@ -1,10 +1,11 @@
 defmodule AutoApi.CustomType.Implementation do
   @moduledoc false
 
-  @external_resource "specs/custom_types.json"
+  @spec_file "specs/misc/custom_types.json"
+  @external_resource @spec_file
 
   defmacro __before_compile__(_env) do
-    specs = Poison.decode!(File.read!("specs/custom_types.json"))
+    specs = Poison.decode!(File.read!(@spec_file))
 
     for type <- specs["types"] do
       name_atom = String.to_atom(type["name"])
