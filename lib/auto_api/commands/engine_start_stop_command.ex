3adcad92d8b4bf0374baf09ff3c7cd641735e36f
Francesco Donadon <francesco.donadon@gmail.com>
2020-05-18 10:05:35 +0200

    Removed EngineStartStopCommand

--- lib/auto_api/commands/engine_start_stop_command.ex
+++
@@ -1,6 +0,0 @@
-defmodule AutoApi.EngineStartStopCommand do
-  @moduledoc """
-  Handles EngineStartStop commands and apply binary commands on `%AutoApi.EngineStartStopState{}`
-  """
-  use AutoApi.Command
-end
