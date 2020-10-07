e894abb024ff3a682c6e4f035f79caade29f39c3
Francesco Donadon <francesco.donadon@gmail.com>
2020-10-07 16:58:50 +0200

    Implemented Unit Types

---
+++ lib/auto_api/unit_type_meta.ex
@@ -0,0 +1,168 @@
+# AutoAPI
+# The MIT License
+#
+# Copyright (c) 2018- High-Mobility GmbH (https://high-mobility.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+defmodule AutoApi.UnitType.Meta do
+  @moduledoc false
+
+  @spec_file "specs/misc/unit_types.json"
+  @external_resource @spec_file
+
+  defmacro __before_compile__(_env) do
+    specs = Jason.decode!(File.read!(@spec_file))
+
+    type_names = for type <- specs["measurement_types"], do: String.to_atom(type["name"])
+
+    base_functions =
+      quote do
+        @type id :: <<_::8>>
+
+        @doc """
+        Returns all measurement types
+
+        # Example
+
+            iex> types = AutoApi.UnitType.all()
+            iex> length(types)
+            18
+            iex> List.first(types)
+            :acceleration
+        """
+        @spec all() :: list(atom())
+        def all(), do: unquote(Macro.escape(type_names))
+
+        @doc """
+        Returns the ID of a measurement type given the name.
+
+        The name can be expressed as a string or atom.
+
+        # Examples
+
+            iex> AutoApi.UnitType.id(:length)
+            0x12
+            iex> AutoApi.UnitType.id("power")
+            0x14
+        """
+        @spec id(String.t() | atom()) :: id()
+        def id(name)
+
+        @doc """
+        Returns the name of a measurement type given its ID
+
+        # Example
+
+            iex> AutoApi.UnitType.name(0x18)
+            :torque
+        """
+        @spec name(id()) :: atom()
+        def name(id)
+
+        @doc """
+        Returns all possible units for the given measurement type.
+
+        The type can be specified either by ID or name (string or atom).
+
+        # Examples
+
+            iex> AutoApi.UnitType.units(:speed) |> List.first()
+            :meters_per_second
+            iex> AutoApi.UnitType.units("torque") |> List.first()
+            :newton_meters
+            iex> AutoApi.UnitType.units(0x11) |> List.first()
+            :lux
+        """
+        @spec units(atom() | String.t() | id()) :: list(atom())
+        def units(name_or_id)
+
+        @doc """
+        Returns the ID of an unit of measurement.
+
+        The measurement name can be passed either as string or atom, but the unit name is only accepted in atom format.
+
+        # Examples
+
+            iex> AutoApi.UnitType.unit_id(:volume, :liters)
+            0x02
+            iex> AutoApi.UnitType.unit_id("power", :megawatts)
+            0x03
+        """
+        @spec unit_id(String.t() | atom(), atom()) :: id()
+        def unit_id(type_name, unit_name)
+
+        @doc """
+        Returns the name of an unit of measurement.
+
+        Both the type and unit must be specified by their IDs.
+
+        # Example
+
+            iex> AutoApi.UnitType.unit_name(0x16, 0x02)
+            :miles_per_hour
+        """
+        @spec unit_name(id(), id()) :: atom()
+        def unit_name(type_id, unit_id)
+      end
+
+    types_functions =
+      for type <- specs["measurement_types"] do
+        name_atom = String.to_atom(type["name"])
+        name_string = type["name"]
+        id = type["id"]
+
+        units = for unit <- type["unit_types"], do: String.to_atom(unit["name"])
+
+        unit_names =
+          for unit <- type["unit_types"],
+              into: %{},
+              do: {unit["id"], String.to_atom(unit["name"])}
+
+        unit_ids =
+          for unit <- type["unit_types"],
+              into: %{},
+              do: {String.to_atom(unit["name"]), unit["id"]}
+
+        quote do
+          # Here we could expand atom() to the actual values instead
+          @type unquote(name_atom)() :: {float(), atom()}
+
+          @units unquote(Macro.escape(units))
+          @unit_names unquote(Macro.escape(unit_names))
+          @unit_ids unquote(Macro.escape(unit_ids))
+
+          def id(unquote(name_atom)), do: unquote(id)
+          def id(unquote(name_string)), do: unquote(id)
+
+          def name(unquote(id)), do: unquote(name_atom)
+
+          def units(unquote(id)), do: @units
+          def units(unquote(name_atom)), do: @units
+          def units(unquote(name_string)), do: @units
+
+          def unit_id(unquote(name_atom), name), do: @unit_ids[name]
+          def unit_id(unquote(name_string), name), do: @unit_ids[name]
+
+          def unit_name(unquote(id), unit_id), do: @unit_names[unit_id]
+        end
+      end
+
+    [base_functions, types_functions]
+  end
+end
---
+++ test/auto_api/unit_type_test.exs
@@ -0,0 +1,198 @@
+# AutoAPI
+# Copyright (C) 2018 High-Mobility GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# Please inquire about commercial licensing options at
+# licensing@high-mobility.com
+defmodule AutoApi.UnitTypeStateTest do
+  use ExUnit.Case, async: true
+  doctest AutoApi.UnitType
+
+  alias AutoApi.UnitType
+
+  setup_all do
+    specs = Jason.decode!(File.read!("specs/misc/unit_types.json"), keys: :atoms)
+
+    {:ok, types: specs.measurement_types}
+  end
+
+  test "all/0" do
+    assert UnitType.all() == [
+             :acceleration,
+             :angle,
+             :angular_velocity,
+             :duration,
+             :electric_current,
+             :electric_potential_difference,
+             :energy,
+             :energy_efficiency,
+             :frequency,
+             :fuel_efficiency,
+             :illuminance,
+             :length,
+             :power,
+             :pressure,
+             :speed,
+             :temperature,
+             :torque,
+             :volume
+           ]
+  end
+
+  describe "id/1" do
+    test "works for all measurement names as strings", %{types: types} do
+      for type <- types do
+        assert type.id == UnitType.id(type.name)
+      end
+    end
+
+    test "works for all measurement names as atoms", %{types: types} do
+      for type <- types do
+        assert type.id == UnitType.id(String.to_atom(type.name))
+      end
+    end
+  end
+
+  describe "name/1" do
+    test "works for all measurement ids", %{types: types} do
+      for type <- types do
+        assert type.name == to_string(UnitType.name(type.id))
+      end
+    end
+  end
+
+  describe "unit_id/2" do
+    test "works for all measurement names", %{types: types} do
+      for type <- types, unit <- type.unit_types do
+        assert unit.id == UnitType.unit_id(String.to_atom(type.name), String.to_atom(unit.name))
+      end
+    end
+
+    test "works for all measurement names as strings", %{types: types} do
+      for type <- types, unit <- type.unit_types do
+        assert unit.id == UnitType.unit_id(type.name, String.to_atom(unit.name))
+      end
+    end
+  end
+
+  describe "unit_name/2" do
+    test "works for all measurement ids", %{types: types} do
+      for type <- types, unit <- type.unit_types do
+        assert String.to_atom(unit.name) == UnitType.unit_name(type.id, unit.id)
+      end
+    end
+  end
+
+  describe "units/1" do
+    test "reads everything correctly from specs" do
+      assert units = UnitType.units(:acceleration)
+      assert length(units) == 2
+      assert List.first(units) == :meters_per_second_squared
+
+      assert units = UnitType.units(:angle)
+      assert length(units) == 3
+      assert List.first(units) == :degrees
+
+      assert units = UnitType.units(:angular_velocity)
+      assert length(units) == 3
+      assert List.first(units) == :revolutions_per_minute
+
+      assert units = UnitType.units(:duration)
+      assert length(units) == 6
+      assert List.first(units) == :seconds
+
+      assert units = UnitType.units(:electric_current)
+      assert length(units) == 3
+      assert List.first(units) == :amperes
+
+      assert units = UnitType.units(:electric_potential_difference)
+      assert length(units) == 3
+      assert List.first(units) == :volts
+
+      assert units = UnitType.units(:energy)
+      assert length(units) == 3
+      assert List.first(units) == :joules
+
+      assert units = UnitType.units(:energy_efficiency)
+      assert length(units) == 2
+      assert List.first(units) == :kwh_per_100_kilometers
+
+      assert units = UnitType.units(:frequency)
+      assert length(units) == 6
+      assert List.first(units) == :hertz
+
+      assert units = UnitType.units(:fuel_efficiency)
+      assert length(units) == 3
+      assert List.first(units) == :liters_per_100_kilometers
+
+      assert units = UnitType.units(:illuminance)
+      assert length(units) == 1
+      assert List.first(units) == :lux
+
+      assert units = UnitType.units(:length)
+      assert length(units) == 12
+      assert List.first(units) == :meters
+
+      assert units = UnitType.units(:acceleration)
+      assert length(units) == 2
+      assert List.first(units) == :meters_per_second_squared
+
+      assert units = UnitType.units(:power)
+      assert length(units) == 5
+      assert List.first(units) == :watts
+
+      assert units = UnitType.units(:pressure)
+      assert length(units) == 7
+      assert List.first(units) == :newtons_per_meters_squared
+
+      assert units = UnitType.units(:speed)
+      assert length(units) == 4
+      assert List.first(units) == :meters_per_second
+
+      assert units = UnitType.units(:temperature)
+      assert length(units) == 3
+      assert List.first(units) == :kelvin
+
+      assert units = UnitType.units(:torque)
+      assert length(units) == 3
+      assert List.first(units) == :newton_meters
+
+      assert units = UnitType.units(:volume)
+      assert length(units) == 14
+      assert List.first(units) == :liters
+    end
+
+    test "works for all measurement ids", %{types: types} do
+      for type <- types do
+        units = Enum.map(type.unit_types, &String.to_atom(&1.name))
+        assert units == UnitType.units(type.id)
+      end
+    end
+
+    test "works for all measurement names", %{types: types} do
+      for type <- types do
+        units = Enum.map(type.unit_types, &String.to_atom(&1.name))
+        assert units == UnitType.units(String.to_atom(type.name))
+      end
+    end
+
+    test "works for all measurement names as strings", %{types: types} do
+      for type <- types do
+        units = Enum.map(type.unit_types, &String.to_atom(&1.name))
+        assert units == UnitType.units(type.name)
+      end
+    end
+  end
+end
