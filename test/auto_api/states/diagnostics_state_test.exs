443f609409230999679e61c0a93ba5af2f3a7e9b
Francesco Donadon <francesco.donadon@gmail.com>
2020-10-20 15:34:18 +0200

    Updated DiagnosticsCapability/State type and tests

--- test/auto_api/states/diagnostics_state_test.exs
+++ test/auto_api/states/diagnostics_state_test.exs
@@ -5,50 +5,92 @@ defmodule AutoApi.DiagnosticsStateTest do
 
   test "to_bin & from_bin" do
     state = %DiagnosticsState{
-      mileage: %PropertyComponent{data: 10},
-      engine_oil_temperature: %PropertyComponent{data: 2},
-      speed: %PropertyComponent{data: 3},
-      engine_rpm: %PropertyComponent{data: 4},
+      mileage: %PropertyComponent{data: {10.0, :miles}},
+      engine_oil_temperature: %PropertyComponent{data: {2, :kelvin}},
+      speed: %PropertyComponent{data: {3, :meters_per_second}},
+      engine_rpm: %PropertyComponent{data: {4, :degrees_per_second}},
       fuel_level: %PropertyComponent{data: 1.0001},
-      estimated_range: %PropertyComponent{data: 5},
+      estimated_range: %PropertyComponent{data: {5, :kilometers}},
       washer_fluid_level: %PropertyComponent{data: :filled},
-      battery_voltage: %PropertyComponent{data: 1.003},
-      distance_since_reset: %PropertyComponent{data: 6},
-      distance_since_start: %PropertyComponent{data: 7},
-      fuel_volume: %PropertyComponent{data: 1.004},
+      battery_voltage: %PropertyComponent{data: {1.003, :volts}},
+      adblue_level: %PropertyComponent{data: {1.003, :liters}},
+      distance_since_reset: %PropertyComponent{data: {6, :kilometers}},
+      distance_since_start: %PropertyComponent{data: {7, :nautical_miles}},
+      fuel_volume: %PropertyComponent{data: {1.004, :gallons}},
       anti_lock_braking: %PropertyComponent{data: :inactive},
-      engine_coolant_temperature: %PropertyComponent{data: 8},
-      engine_total_operating_hours: %PropertyComponent{data: 1.005},
-      engine_total_fuel_consumption: %PropertyComponent{data: 1.006},
+      engine_coolant_temperature: %PropertyComponent{data: {8, :fahrenheit}},
+      engine_total_operating_hours: %PropertyComponent{data: {1.005, :months}},
+      engine_total_fuel_consumption: %PropertyComponent{data: {1.006, :liters}},
       brake_fluid_level: %PropertyComponent{data: :low},
       engine_torque: %PropertyComponent{data: 1.007},
       engine_load: %PropertyComponent{data: 1.008},
-      wheel_based_speed: %PropertyComponent{data: 9},
+      wheel_based_speed: %PropertyComponent{data: {9, :miles_per_hour}},
       battery_level: %PropertyComponent{data: 1.009},
       check_control_messages: [
         %PropertyComponent{
           data: %{
             id: 1,
-            remaining_minutes: 300,
+            remaining_time: {300, :minutes},
             text: "text",
             status: "status"
           }
         }
       ],
-      tire_pressures: [%PropertyComponent{data: %{location: :rear_left, pressure: 1.009}}],
-      tire_temperatures: [%PropertyComponent{data: %{location: :rear_left, temperature: 1.010}}],
-      wheel_rpms: [%PropertyComponent{data: %{location: :rear_right, rpm: 10}}],
+      tire_pressures: [
+        %PropertyComponent{data: %{location: :rear_left, pressure: {1.009, :bars}}}
+      ],
+      tire_temperatures: [
+        %PropertyComponent{data: %{location: :rear_left_outer, temperature: {1.010, :kelvin}}}
+      ],
+      wheel_rpms: [
+        %PropertyComponent{data: %{location: :rear_right, rpm: {10, :revolutions_per_minute}}}
+      ],
       trouble_codes: [
         %PropertyComponent{
           data: %{
-            occurences: 1,
+            occurrences: 1,
             id: "id",
             ecu_id: "ecu_id",
+            status: "status",
+            system: :body
+          }
+        }
+      ],
+      mileage_meters: %PropertyComponent{data: {11, :meters}},
+      odometer: %PropertyComponent{data: {11, :meters}},
+      engine_total_operating_time: %PropertyComponent{data: {11, :hours}},
+      tire_pressure_statuses: [
+        %PropertyComponent{data: %{location: :rear_left, status: :alert}}
+      ],
+      brake_lining_wear_pre_warning: %PropertyComponent{data: :active},
+      engine_oil_life_remaining: %PropertyComponent{data: 45.56},
+      oem_trouble_code_values: [
+        %PropertyComponent{
+          data: %{
+            id: "id",
+            key_value: %{key: "ecu_id", value: "status"}
+          }
+        }
+      ],
+      diesel_exhaust_fluid_range: %PropertyComponent{data: {11, :meters}},
+      diesel_particulate_filter_soot_level: %PropertyComponent{data: 11.78},
+      confirmed_trouble_codes: [
+        %PropertyComponent{
+          data: %{
+            id: "id",
+            ecu_address: "address",
+            ecu_variant_name: "variant",
             status: "status"
           }
         }
       ],
-      mileage_meters: %PropertyComponent{data: 11}
+      diesel_exhaust_filter_status: %PropertyComponent{
+        data: %{
+          status: :at_limit,
+          component: :diesel_particulate_filter,
+          cleaning: :interrupted
+        }
+      }
     }
 
     new_state =
