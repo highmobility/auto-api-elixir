0870d42bbdf62db6c0b21bd12db599e6fff03423
Francesco Donadon <francesco.donadon@gmail.com>
2020-10-22 12:38:53 +0200

    Update Maintenance

--- lib/auto_api/states/maintenance_state.ex
+++ lib/auto_api/states/maintenance_state.ex
@@ -25,39 +25,41 @@ defmodule AutoApi.MaintenanceState do
   Maintenance state
   """
 
-  alias AutoApi.{CommonData, PropertyComponent}
+  alias AutoApi.{CommonData, State, UnitType}
 
   use AutoApi.State, spec_file: "maintenance.json"
 
-  @type condition_based_services :: %PropertyComponent{
-          data: %{
-            year: integer,
-            month: integer,
-            identifier: integer,
-            due_status: :ok | :pending | :overdue,
-            text_size: integer,
-            text: String.t(),
-            description_size: integer,
-            description: String.t()
-          }
+  @type condition_based_services :: %{
+          year: integer,
+          month: integer,
+          identifier: integer,
+          due_status: :ok | :pending | :overdue,
+          text: String.t(),
+          description: String.t()
         }
 
   @type activity :: :inactive | :active
   @type teleservice_availability :: :pending | :idle | :successful | :error
 
   @type t :: %__MODULE__{
-          days_to_next_service: %PropertyComponent{data: integer} | nil,
-          kilometers_to_next_service: %PropertyComponent{data: integer} | nil,
-          cbs_reports_count: %PropertyComponent{data: integer} | nil,
-          months_to_exhaust_inspection: %PropertyComponent{data: integer} | nil,
-          teleservice_availability: %PropertyComponent{data: teleservice_availability} | nil,
-          service_distance_threshold: %PropertyComponent{data: integer} | nil,
-          service_time_threshold: %PropertyComponent{data: integer} | nil,
-          automatic_teleservice_call_date: %PropertyComponent{data: integer} | nil,
-          teleservice_battery_call_date: %PropertyComponent{data: integer} | nil,
-          next_inspection_date: %PropertyComponent{data: integer} | nil,
-          condition_based_services: list(condition_based_services),
-          brake_fluid_change_date: %PropertyComponent{data: integer} | nil
+          # Deprecated
+          days_to_next_service: State.property(UnitType.duration()),
+          # Deprecated
+          kilometers_to_next_service: State.property(UnitType.length()),
+          cbs_reports_count: State.property(integer),
+          # Deprecated
+          months_to_exhaust_inspection: State.property(UnitType.duration()),
+          teleservice_availability: State.property(teleservice_availability),
+          service_distance_threshold: State.property(UnitType.length()),
+          service_time_threshold: State.property(UnitType.duration()),
+          automatic_teleservice_call_date: State.property(DateTime.t()),
+          teleservice_battery_call_date: State.property(DateTime.t()),
+          next_inspection_date: State.property(DateTime.t()),
+          condition_based_services: State.multiple_property(condition_based_services),
+          brake_fluid_change_date: State.property(DateTime.t()),
+          distance_to_next_service: State.property(UnitType.length()),
+          time_to_exhaust_inspection: State.property(UnitType.duration()),
+          last_ecall: State.property(DateTime.t())
         }
 
   @doc """
@@ -65,8 +67,8 @@ defmodule AutoApi.MaintenanceState do
 
   ## Example
 
-      iex> AutoApi.MaintenanceState.from_bin(<<0x02, 7::integer-16, 0x01, 4::integer-16, -42::integer-32>>)
-      %AutoApi.MaintenanceState{kilometers_to_next_service: %AutoApi.PropertyComponent{data: -42}}
+      iex> AutoApi.MaintenanceState.from_bin(<<3, 0, 4, 1, 0, 1, 42>>)
+      %AutoApi.MaintenanceState{cbs_reports_count: %AutoApi.PropertyComponent{data: 42}}
   """
   @spec from_bin(binary) :: __MODULE__.t()
   def from_bin(bin) do
@@ -78,9 +80,9 @@ defmodule AutoApi.MaintenanceState do
 
   ## Example
 
-      iex> state = %AutoApi.MaintenanceState{kilometers_to_next_service: %AutoApi.PropertyComponent{data: -42}}
+      iex> state = %AutoApi.MaintenanceState{cbs_reports_count: %AutoApi.PropertyComponent{data: 42}}
       iex> AutoApi.MaintenanceState.to_bin(state)
-      <<0x02, 7::integer-16, 0x01, 4::integer-16, -42::integer-32>>
+      <<3, 0, 4, 1, 0, 1, 42>>
   """
   @spec to_bin(__MODULE__.t()) :: binary
   def to_bin(%__MODULE__{} = state) do
