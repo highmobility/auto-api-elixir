fba53052bd9159fabe9a0b4db4f014f8ce279dc9
Francesco Donadon <francesco.donadon@gmail.com>
2020-10-23 08:19:24 +0200

    Remove old, disabled tests

--- test/auto_api/commands/charging_command_test.exs
+++ test/auto_api/commands/charging_command_test.exs
@@ -1,32 +1,4 @@
 defmodule AutoApi.ChargingCommandTest do
   use ExUnit.Case
-  alias AutoApi.{ChargingCommand, ChargingState, PropertyComponent}
-
-  describe "execute/2" do
-    # TODO
-    @tag :skip
-    test "get_charge_state" do
-      assert ChargingCommand.execute(%ChargingState{}, <<0x00>>) == {:state, %ChargingState{}}
-    end
-
-    # TODO
-    @tag :skip
-    test "charge_state" do
-      estimated_range_prop = %PropertyComponent{data: 100}
-      binary_state = ChargingState.to_bin(%ChargingState{estimated_range: estimated_range_prop})
-
-      assert ChargingCommand.execute(%ChargingState{}, <<0x01, binary_state::binary>>) ==
-               {:state_changed, %ChargingState{estimated_range: estimated_range_prop}}
-    end
-  end
-
-  describe "state/1" do
-    # TODO
-    @tag :skip
-    test "get state" do
-      state = %ChargingState{estimated_range: %PropertyComponent{data: 100}}
-
-      assert ChargingCommand.state(state) == <<0x01>> <> ChargingState.to_bin(state)
-    end
-  end
+  doctest AutoApi.ChargingCommand
 end
--- test/auto_api/commands/doors_command_test.exs
+++ test/auto_api/commands/doors_command_test.exs
@@ -1,42 +1,4 @@
 defmodule AutoApi.DoorsCommandTest do
   use ExUnit.Case
-  alias AutoApi.{DoorsCommand, DoorsState, PropertyComponent}
-
-  describe "execute/2" do
-    # TODO
-    @tag :skip
-    test "lock_unlock_doors lock command" do
-      command = DoorsCommand.to_bin(:set, lock_state: :lock)
-
-      state =
-        DoorsState.append_property(%DoorsState{}, :locks, %{
-          door_location: :front_right,
-          lock_state: :unlocked
-        })
-
-      assert {:state_changed, new_state} = DoorsCommand.execute(state, command)
-
-      assert new_state.locks == [
-               %PropertyComponent{data: %{door_location: :front_right, lock_state: :locked}}
-             ]
-    end
-
-    # TODO
-    @tag :skip
-    test "lock_unlock_doors unlock command" do
-      command = DoorsCommand.to_bin(:set, lock_state: :unlock)
-
-      state =
-        DoorsState.append_property(%DoorsState{}, :locks, %{
-          door_location: :front_right,
-          lock_state: :locked
-        })
-
-      assert {:state_changed, new_state} = DoorsCommand.execute(state, command)
-
-      assert new_state.locks == [
-               %PropertyComponent{data: %{door_location: :front_right, lock_state: :unlocked}}
-             ]
-    end
-  end
+  doctest AutoApi.DoorsCommand
 end
